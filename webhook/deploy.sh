7fa45cb36a74e301737266f1b44a0875e3d55fa8
anytao <twang281314@gmail.com>
2017-12-25 16:23:52 +0800

    update deploy.sh

--- webhook/deploy.sh
+++ webhook/deploy.sh
@@ -1,7 +1,20 @@
-WEB_PATH='/usr/coding.net/blog/'$1
+#!/bin/bash
+
+WEB_PATH='/usr/github/blog/'$1
 WEB_USER='root'
 WEB_USERGROUP='root'
 
 echo "Start deployment"
-# cd $WEB_PATH
+cd $WEB_PATH
 echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Start compile"
+cnpm install 
+npm run compile
+pm2 restart all
+echo "Finished."
